61c9ccfa24f4d59657f67aebab5137597999d4bd
frangherpereira <frangher200@gmail.com>
2026-06-06 09:11:16 -0400

    modulo gerencia vista

--- my-app/conexion/conexionBD.py
+++ my-app/conexion/conexionBD.py
@@ -11,7 +11,7 @@ def connectionBD():
         connection = mysql.connector.connect(
             host=os.getenv('DB_HOST', 'localhost'),
             user=os.getenv('DB_USER', 'root'),
-            password=os.getenv('DB_PASSWORD', ''),
+            password=os.getenv('DB_PASSWORD', '1234'),
             database=os.getenv('DB_NAME', 'crud_python'),
             charset='utf8mb4'
         )
