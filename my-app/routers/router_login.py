80a909a3ac19d5ddba7f3440adcb879bb6623d4d
David Peña <davidalejandropegaso@gmail.com>
2026-06-06 17:02:55 -0400

    sidebar

--- my-app/routers/router_login.py
+++ my-app/routers/router_login.py
@@ -1,6 +1,4 @@
-
-from app import app
-from flask import render_template, request, flash, redirect, url_for, session
+from flask import Blueprint, render_template, request, flash, redirect, url_for, session
 
 # Importando mi conexión a BD
 from conexion.conexionBD import connectionBD
@@ -10,10 +8,12 @@ from werkzeug.security import check_password_hash
 
 # Importando controllers para el modulo de login
 from controllers.funciones_login import *
+
+login_bp = Blueprint('login_bp', __name__)
 PATH_URL_LOGIN = "login"
 
 
-@app.route('/', methods=['GET'])
+@login_bp.route('/', methods=['GET'])
 def inicio():
     if 'conectado' in session:
         return render_template('base_cpanel.html', dataLogin=dataLoginSesion())
@@ -21,34 +21,34 @@ def inicio():
         return render_template(f'{PATH_URL_LOGIN}/base_login.html')
 
 
-@app.route('/mi-perfil', methods=['GET'])
+@login_bp.route('/mi-perfil', methods=['GET'])
 def perfil():
     if 'conectado' in session:
         return render_template(f'perfil/perfil.html', info_perfil_session=info_perfil_session())
     else:
-        return redirect(url_for('inicio'))
+        return redirect(url_for('login_bp.inicio'))
 
 
 # Crear cuenta de usuario
-@app.route('/register-user', methods=['GET'])
+@login_bp.route('/register-user', methods=['GET'])
 def cpanelRegisterUser():
     if 'conectado' in session:
-        return redirect(url_for('inicio'))
+        return redirect(url_for('login_bp.inicio'))
     else:
         return render_template(f'{PATH_URL_LOGIN}/auth_register.html')
 
 
 # Recuperar cuenta de usuario
-@app.route('/recovery-password', methods=['GET'])
+@login_bp.route('/recovery-password', methods=['GET'])
 def cpanelRecoveryPassUser():
     if 'conectado' in session:
-        return redirect(url_for('inicio'))
+        return redirect(url_for('login_bp.inicio'))
     else:
         return render_template(f'{PATH_URL_LOGIN}/auth_forgot_password.html')
 
 
 # Crear cuenta de usuario
-@app.route('/saved-register', methods=['POST'])
+@login_bp.route('/saved-register', methods=['POST'])
 def cpanelResgisterUserBD():
     if request.method == 'POST' and 'name_surname' in request.form and 'pass_user' in request.form:
         name_surname = request.form['name_surname']
@@ -59,23 +59,23 @@ def cpanelResgisterUserBD():
             name_surname, email_user, pass_user)
         if (resultData != 0):
             flash('la cuenta fue creada correctamente.', 'success')
-            return redirect(url_for('inicio'))
+            return redirect(url_for('login_bp.inicio'))
         else:
-            return redirect(url_for('inicio'))
+            return redirect(url_for('login_bp.inicio'))
     else:
         flash('el método HTTP es incorrecto', 'error')
-        return redirect(url_for('inicio'))
+        return redirect(url_for('login_bp.inicio'))
 
 
 # Actualizar datos de mi perfil
-@app.route("/actualizar-datos-perfil", methods=['POST'])
+@login_bp.route("/actualizar-datos-perfil", methods=['POST'])
 def actualizarPerfil():
     if request.method == 'POST':
         if 'conectado' in session:
             respuesta = procesar_update_perfil(request.form)
             if respuesta == 1:
                 flash('Los datos fuerón actualizados correctamente.', 'success')
-                return redirect(url_for('inicio'))
+                return redirect(url_for('login_bp.inicio'))
             elif respuesta == 0:
                 flash(
                     'La contraseña actual esta incorrecta, por favor verifique.', 'error')
@@ -88,17 +88,17 @@ def actualizarPerfil():
                 return redirect(url_for('perfil'))
         else:
             flash('primero debes iniciar sesión.', 'error')
-            return redirect(url_for('inicio'))
+            return redirect(url_for('login_bp.inicio'))
     else:
         flash('primero debes iniciar sesión.', 'error')
-        return redirect(url_for('inicio'))
+        return redirect(url_for('login_bp.inicio'))
 
 
 # Validar sesión
-@app.route('/login', methods=['GET', 'POST'])
+@login_bp.route('/login', methods=['GET', 'POST'])
 def loginCliente():
     if 'conectado' in session:
-        return redirect(url_for('inicio'))
+        return redirect(url_for('login_bp.inicio'))
     else:
         if request.method == 'POST' and 'email_user' in request.form and 'pass_user' in request.form:
 
@@ -130,7 +130,7 @@ def loginCliente():
                     session['email_user'] = account['email_user']
 
                     flash('la sesión fue correcta.', 'success')
-                    return redirect(url_for('inicio'))
+                    return redirect(url_for('login_bp.inicio'))
                 else:
                     # La cuenta no existe o el nombre de usuario/contraseña es incorrecto
                     flash('datos incorrectos por favor revise.', 'error')
@@ -143,17 +143,17 @@ def loginCliente():
             return render_template(f'{PATH_URL_LOGIN}/base_login.html')
 
 
-@app.route('/closed-session',  methods=['GET'])
-def cerraSesion():
+@login_bp.route('/closed-session',  methods=['GET'])
+def logout():
     if request.method == 'GET':
         if 'conectado' in session:
             # Eliminar datos de sesión, esto cerrará la sesión del usuario
             session.pop('conectado', None)
             session.pop('id', None)
             session.pop('name_surname', None)
-            session.pop('email', None)
+            session.pop('email_user', None)
             flash('tu sesión fue cerrada correctamente.', 'success')
-            return redirect(url_for('inicio'))
+            return redirect(url_for('login_bp.inicio'))
         else:
             flash('recuerde debe iniciar sesión.', 'error')
             return render_template(f'{PATH_URL_LOGIN}/base_login.html')
