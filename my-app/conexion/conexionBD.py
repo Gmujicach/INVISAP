020975318b98d36c09a6de186e89626e9e5ff214
David Peña <davidalejandropegaso@gmail.com>
2026-06-06 01:39:41 -0400

    empleados

--- my-app/conexion/conexionBD.py
+++ my-app/conexion/conexionBD.py
@@ -21,7 +21,7 @@ def connectionBD():
         connection = mysql.connector.connect(
             host=os.getenv('DB_HOST', 'localhost'),
             user=os.getenv('DB_USER', 'root'),
-            password=os.getenv('DB_PASSWORD', '1234'),
+            password=os.getenv('DB_PASSWORD', ''),
             database=os.getenv('DB_NAME', 'crud_python'),
             charset='utf8mb4'
         )
